1daa5346670246d24c0601354cfe268c70d13c7a
eilaradam <laradam.ugc@gmail.com>
2026-09-19 12:32:21 -0300

    Fix: Configure basePath for GitHub Pages deployment at /ugc321

---
+++ static/o22KvR5I4SZeZ0nwS-6-K/_buildManifest.js
@@ -0,0 +1,36 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [
+      {
+        "source": "/ugc321/bio",
+        "destination": "/ugc321/bio/index.html"
+      },
+      {
+        "source": "/ugc321/bio/",
+        "destination": "/ugc321/bio/index.html"
+      },
+      {
+        "source": "/ugc321/replit",
+        "destination": "/ugc321/replit/index.html"
+      },
+      {
+        "source": "/ugc321/replit/",
+        "destination": "/ugc321/replit/index.html"
+      },
+      {
+        "source": "/ugc321/projeto/3d",
+        "destination": "/ugc321/projeto/3d/index.html"
+      },
+      {
+        "source": "/ugc321/projeto/3d/",
+        "destination": "/ugc321/projeto/3d/index.html"
+      }
+    ],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
